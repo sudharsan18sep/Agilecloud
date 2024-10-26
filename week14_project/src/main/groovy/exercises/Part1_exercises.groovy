ad786fa673baa93c70d1e43fd7fff589224a2f0f
meharfatimak <mehar.fatima@tintash.com>
2024-10-26 22:22:10 +0100

    added changes for data projection, filtering and combination for exercise3

--- week14_project/src/main/groovy/exercises/Part1_exercises.groovy
+++ week14_project/src/main/groovy/exercises/Part1_exercises.groovy
@@ -1,6 +1,7 @@
 package exercises
 
 import static com.mongodb.client.model.Accumulators.*
+
 import static com.mongodb.client.model.Aggregates.*
 import static com.mongodb.client.model.Filters.*
 import static com.mongodb.client.model.Sorts.*
