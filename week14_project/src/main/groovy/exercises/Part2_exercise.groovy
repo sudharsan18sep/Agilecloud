35bd9143e0914c930ac4dea76d0fd79ae3a9e5ff
meharfatimak <mehar.fatima@tintash.com>
2024-10-26 20:14:17 +0100

    fix: Add logic for uploading collection

--- week14_project/src/main/groovy/exercises/Part2_exercise.groovy
+++ week14_project/src/main/groovy/exercises/Part2_exercise.groovy
@@ -1,8 +1,11 @@
 package exercises
 
 import org.bson.Document
+
 import com.mongodb.client.MongoClients
 import static com.mongodb.client.model.Filters.*
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
 
 // Load credentials from src/main/resources/mongodb.properties
 def properties = new Properties()
@@ -11,11 +14,31 @@ propertiesFile.withInputStream {
     properties.load(it)
 }
 
+def uploadCollection(collection, src) {
+	def parser = new JsonSlurper()
+	// parse JSON file
+	def file = new File(src)
+	def doc = parser.parse(file)
+	
+	int i = 0
+	doc.each{
+		println ""
+		println "object ${i++}: $it"
+		document = new Document();
+		document.putAll(it)
+		collection.insertOne(document)
+	}
+}
+
 // connection string to connect to the primary node to make write operations
 String connectionString = "mongodb+srv://${properties.USN}:${properties.PWD}@cluster0.${properties.SERVER}.mongodb.net/${properties.DB}?retryWrites=true&w=majority&appName=${properties.APP_NAME}";
 
 def mongoClient = MongoClients.create(connectionString)
 def db = mongoClient.getDatabase(properties.DB)
 def airbnb_collection = db.getCollection("listingsAndReviews")
+def country_gni_collection = db.getCollection("country_gni")
 
-println airbnb_collection.find().first()
+// upload method for upload collection to mongodb atlas
+//uploadCollection(country_gni_collection, 'src/main/resources/country_gni.json')
+
+
